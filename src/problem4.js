874c9339304bb759e01b58a4d34dfaee4993ea4d
shackstack <kms1061@naver.com>
2022-10-26 20:05:52 +0900

    [Feat] problem4

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,15 @@
 function problem4(word) {
-  var answer;
+  let answer;
+  let splitWord = word.split("");
+
+  answer = splitWord.map((element) => {
+    if (65 <= element.charCodeAt() && element.charCodeAt() <= 90) {
+      return String.fromCharCode(90 - element.charCodeAt() + 65);
+    } else if (97 <= element.charCodeAt() && element.charCodeAt() <= 122) {
+      return String.fromCharCode(122 - element.charCodeAt() + 97);
+    } else return element;
+  });
+
   return answer;
 }
 
