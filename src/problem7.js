a0742e936bfba830d06862bad77a4d55286b2c18
shackstack <kms1061@naver.com>
2022-10-27 20:53:21 +0900

    [Feat] problem7

--- src/problem7.js
+++ src/problem7.js
@@ -1,5 +1,52 @@
 function problem7(user, friends, visitors) {
-  var answer;
+  let userData = {};
+  friends.forEach((item) => {
+    if (!userData[item[0]]) {
+      userData[item[0]] = [];
+    }
+    userData[item[0]].push(item[1]);
+  });
+  friends.forEach((item) => {
+    if (!userData[item[1]]) {
+      userData[item[1]] = [];
+    }
+    userData[item[1]].push(item[0]);
+  });
+
+  let friendOfFriend = [];
+  userData[user].forEach((friend) => {
+    userData[friend].forEach((friendsFriend) => {
+      if (friendsFriend != user) {
+        friendOfFriend.push(friendsFriend);
+      }
+    });
+  });
+
+  let pointInfo = {};
+  friendOfFriend.forEach((person) => {
+    if (!pointInfo[person]) {
+      pointInfo[person] = 0;
+    }
+    pointInfo[person] += 10;
+  });
+  visitors.forEach((person) => {
+    if (!userData[user].includes(person)) {
+      if (!pointInfo[person]) {
+        pointInfo[person] = 0;
+      }
+      pointInfo[person] += 1;
+    }
+  });
+
+  let arr = Object.entries(pointInfo);
+  arr.sort();
+  arr.sort((a, b) => {
+    return b[1] - a[1];
+  });
+
+  let answer = [];
+  arr.forEach((item) => answer.push(item[0]));
+
   return answer;
 }
 
