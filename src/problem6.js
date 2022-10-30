040dbcf3969b4fb19b18d27a25d84d841a6464b2
shackstack <kms1061@naver.com>
2022-10-30 14:58:21 +0900

    [Docs] 구현기능목록 리드미 생성 및 문제 1번 목록작성

--- src/problem6.js
+++ src/problem6.js
@@ -29,25 +29,4 @@ function problem6(forms) {
   return answer;
 }
 
-console.log(
-  problem6([
-    ["jm@email.com", "제이엠"],
-    ["jason@email.com", "제이슨"],
-    ["woniee@email.com", "워니"],
-    ["mj@email.com", "엠제이"],
-    ["nowm@email.com", "이제엠"],
-  ])
-);
 module.exports = problem6;
-
-// function user(arr) {
-//   this.email = arr[0];
-//   this.nickname = arr[1];
-// }
-// const userList = forms.map((item) => {
-//   return new user(item);
-// });
-
-// const dupicatedWordList = userList.map((item)=>{
-//   return item.
-// })
