1e244897ce23330989f2757fbb9159066f9ceda3
Jules <dali1999@naver.com>
2023-10-17 06:04:15 +0900

    feat: problem2

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,15 @@
 function problem2(cryptogram) {
-  var answer;
+  const answer = reduceWord(cryptogram);
   return answer;
+}
+
+function reduceWord(word) {
+  let new_word = word;
+  for (let i = 0; i < word.length; i++) {
+    //글자 수 만큼
+    new_word = new_word.replace(/([a-z])\1{1,}/g, "");
+  }
+  return new_word;
 }
 
 module.exports = problem2;
